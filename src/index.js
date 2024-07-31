0538296b80c8ca9951d87ebdda8493b00a1cab34
royalehoneybadger <takethejess@gmail.com>
2024-07-31 10:06:52 +0200

    feat: function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+        let num = n.toString();
+        let reverse = num.split("").reverse().join("");
+        let reversedNum = parseInt(reverse);
+        return reversedNum;
 }
